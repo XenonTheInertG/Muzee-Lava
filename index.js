4e4c473958d57f65f8957fdab9908828725a3aee
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-11-13 17:28:03 +0600

    Use freadboat's latest lavalink

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//Muzee-Lava
 const { default: { stream } } = require("got");
-const { createWriteStream } = require("fs"); 
+const { createWriteStream } = require("fs");
 const { execSync } = require("child_process");
-const url = "https://cdn.darrennathanael.com/jars/Lavalink.jar"; // This will always pull the latest with soundcloud fix ect ect
+
+const url = "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev";
+
 const start = () => {
     const download = stream(url).pipe(createWriteStream('Lavalink.jar'));
     download.on("finish", () => {
